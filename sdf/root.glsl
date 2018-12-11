515a0f4a87f3abe204a9f626b6b9ff51552cd6ec
Carolyn Lu <cludosmit@gmail.com>
2018-12-10 23:20:39 -0500

    begin building code

--- sdf/root.glsl
+++ sdf/root.glsl
@@ -21,6 +21,7 @@ float column(in vec3 p, in float radius, in float height,
             float baseoffset, float baseheight,
             in float ridgeradius, in int numridges) {
     float cylinder = fCylinder(p, radius, height);
+
     if (ridgeradius>0){
         float ridges = columnridges(p, radius, height, ridgeradius, numridges, baseheight);
         cylinder = fOpDifferenceRound(cylinder, ridges, 0.01);
@@ -62,12 +63,40 @@ float window(in vec3 p, in float wall, in float width, in float height, in float
     //return indent;
     //return wall;
     //return dividers;
-    return fOpDifferenceStairs(wall,indent,max(depth*0.3*cos(0.1*time),0.1),3);
+    return indent;//fOpDifferenceStairs(wall,indent,max(depth*0.3*cos(0.1*time),0.1),3);
 }
 
+float subtractblock(in vec3 p){
+    float width = 8;
+    float height = 15;
+    float length = 8;
+    float block = fBox(p, vec3(width, height, length));
+    float windowwidth = 2;
+    float windowheight = 3;
+    float windowdepth = 0.5;
+    bool arc = true;
+    int heightdivisions = 6;
+    int widthdivisions = 3;
 
+    float dividerthickness = 0.05;
+    float betweenwidth = 0.5;
+    float windows = 1e10;
+    for (int i=0; i<4; i++){
+        pR(p.xz, i*3.1414/2);
+        vec3 prepeat = p+vec3(0,+float(arc)*windowwidth/2,+length-windowdepth);
+
+        pMod2(prepeat.xy, vec2(windowwidth*2+2*betweenwidth, (windowheight+windowwidth*float(arc))*2));
+        windows = min(windows, window(prepeat, block, windowwidth, windowheight, windowdepth,
+                               arc, widthdivisions, heightdivisions, dividerthickness));
+
+    }
+    //return max(windows, block);
+    float subblock = fBox(p, vec3(width-windowdepth, height, length-windowdepth));
+    return min(max(windows, block),subblock);
+}
 
 float sdf(in vec3 p) {
+     p = p + vec3(2,0,10);
     // What does this do?
     //pMod3(p, vec3(35.0));
     float radius = max(cos(time),0.5);//0.5;
@@ -91,5 +120,7 @@ float sdf(in vec3 p) {
     float wall = fBox(p+vec3(0,0,3), vec3(6,8,3));
     float windowobj = window(p, wall, windowwidth, windowheight, windowdepth, arc, widthdivisions, heightdivisions, dividerthickness);
     //return windowobj;
-    return wall;
+    //return wall;
+    //return subtractblock(p-vec3(0,0,5));
+    return max(fBox(p,vec3(10,20,5)),-subtractblock(p-vec3(0,0,5)));
 }
